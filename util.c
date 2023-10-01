6e377f540b56baf814498494c510962a342953cb
StenioEric <stenioeric@gmail.com>
2023-10-01 14:57:44 -0300

    Criado arquivo util.c e util.h

---
+++ util.c
@@ -0,0 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Serido                     ///
+///             PROJETO SIG DE UMA LOJA PARA ARTIGOS FEMININOS              ///
+///             Developed by @stenioeric -- since August, 2023              ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
