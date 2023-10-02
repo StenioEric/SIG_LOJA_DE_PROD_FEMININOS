4463f307739d60ffc5ce129465c9ba9fbc4377a6
StenioEric <stenioeric@gmail.com>
2023-10-02 13:46:51 -0300

    Adicionada validações para módulo cliente

--- util.c
+++ util.c
@@ -7,5 +7,201 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
+#include "cliente.h"
+#include "util.h"
 
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se o caractere recebido for uma alfabético 
+/// (letra entre 'A' e 'Z' ou 'a' e 'z') ou retorna 0 caso contrário
+///
+int ehLetra(char c) {
+  if (c >= 'A' && c <= 'Z') {
+    return 1;
+  } else if (c >= 'a' && c <= 'z') {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se string recebido for exclusivamente alfabético ou
+/// retorna 0 caso contrário
+///
+int validarNome(char* nome) {
+  for (int i=0; nome[i]!='\0'; i++) {
+    if (!ehLetra(nome[i])) {
+      return 0;
+    }
+  }
+	return 1;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se dia, mes e ano correspondem a uma data válida, inclusive
+/// em anos bissextos, ou retorna 0 caso contrário
+///
+int ehData(int dd, int mm, int aa) {
+  int maiorDia;
+  if (aa < 0 || mm < 1 || mm > 12)
+    return 0;
+  if (mm == 2) {
+    if (ehBissexto(aa)) 
+      maiorDia = 29;
+    else
+      maiorDia = 28;
+  } else if (mm == 4 || mm == 6 || mm == 9 || mm == 11) {
+    maiorDia = 30;
+  } else
+    maiorDia = 31;
+  if (dd < 1 || dd > maiorDia)
+    return 0;
+  return 1;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se string recebido corresponder a uma data válida (apenas dígitos
+/// e no formato: ddmmaaaa) ou retorna 0 caso contrário
+///
+int validarData(char* data) {
+  int tam, dia, mes, ano;
+  tam = strlen(data);
+  if (tam != 8) {
+    return 0;
+  }
+  for (int i = 0; i < tam; i++) {
+    if (!ehDigito(data[i])) {
+      return 0;
+    }
+  }
+  dia = (data[0] - '0') * 10 + (data[1] - '0');
+  mes = (data[2] - '0') * 10 + (data[3] - '0');
+  ano = (data[4] - '0') * 1000 + (data[5] - '0') * 100 + 
+        (data[6] - '0') * 10 + (data[7] - '0');
+  if (!ehData(dia, mes, ano)) {
+    return 0;
+  }
+  return 1;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se string recebido corresponder a um número de celular válido 
+/// (apenas dígitos) ou retorna 0 caso contrário
+///
+int validarFone(char* fone) {
+  int tam;
+  tam = strlen(fone);
+  if (tam != 11) {
+    return 0;
+  }
+  for (int i = 0; i < tam; i++) {
+    if (!ehDigito(fone[i])) {
+        return 0;
+    }
+  }
+  return 1;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se ano for bissexto (divisível por 4, não divisível por ...
+/// 100 ou divisível por 400) e retorna 0 caso contrário
+///
+int ehBissexto(int aa) {
+  if ((aa % 4 == 0) && (aa % 100 != 0)) {
+    return 1;
+  } else if (aa % 400 == 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se string recebido corresponder a um cpf válido 
+/// ou retorna 0 caso contrário
+///
+int validarCPF(char *cpf)
+{
+    int i, j, digito1 = 0, digito2 = 0;
+    if(strlen(cpf) != 11)
+        return 0;
+    else if((strcmp(cpf,"00000000000") == 0) || (strcmp(cpf,"11111111111") == 0) || (strcmp(cpf,"22222222222") == 0) ||
+            (strcmp(cpf,"33333333333") == 0) || (strcmp(cpf,"44444444444") == 0) || (strcmp(cpf,"55555555555") == 0) ||
+            (strcmp(cpf,"66666666666") == 0) || (strcmp(cpf,"77777777777") == 0) || (strcmp(cpf,"88888888888") == 0) ||
+            (strcmp(cpf,"99999999999") == 0))
+        return 0; ///se o CPF tiver todos os números iguais ele é inválido.
+    else
+    {
+        ///digito 1---------------------------------------------------
+        for(i = 0, j = 10; i < strlen(cpf)-2; i++, j--) ///multiplica os números de 10 a 2 e soma os resultados dentro de digito1
+            digito1 += (cpf[i]-48) * j;
+        digito1 %= 11;
+        if(digito1 < 2)
+            digito1 = 0;
+        else
+            digito1 = 11 - digito1;
+        if((cpf[9]-48) != digito1)
+            return 0; ///se o digito 1 não for o mesmo que o da validação CPF é inválido
+        else
+        ///digito 2--------------------------------------------------
+        {
+            for(i = 0, j = 11; i < strlen(cpf)-1; i++, j--) ///multiplica os números de 11 a 2 e soma os resultados dentro de digito2
+                    digito2 += (cpf[i]-48) * j;
+        digito2 %= 11;
+        if(digito2 < 2)
+            digito2 = 0;
+        else
+            digito2 = 11 - digito2;
+        if((cpf[10]-48) != digito2)
+            return 0; ///se o digito 2 não for o mesmo que o da validação CPF é inválido
+        }
+    }
+    return 1;
+}  // AUTOR: EDUARDO EDSON /// GIT: https://gist.github.com/eduardoedson
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se string recebido corresponder a um email válido 
+/// ou retorna 0 caso contrário
+///
+int valEmail(char *email )
+{
+    char usuario[256], site[256], dominio[256];
+    if( sscanf( email, "%[^@ \t\n]@%[^. \t\n].%3[^ \t\n]", usuario, site, dominio ) != 3 )
+        return 0;
+    return 1;
+}  // AUTOR: LACOBUS /// STACKOVERFLOW: https://pt.stackoverflow.com/questions/310096/como-validar-um-e-mail-em-c
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se string recebido corresponder a um email válido 
+/// ou retorna 0 caso contrário
+///
+void limparBuffer(void) {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se o caractere recebido for um dígito (entre 0 e 9)
+/// retorna 0 caso contrário
+///
+int ehDigito(char c) {
+  if (c >= '0' && c <= '9') {
+    return 1;
+  } else {
+    return 0;
+  }
+}
