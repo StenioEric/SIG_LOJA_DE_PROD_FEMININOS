1275a5ee00bbf932f354e344841a852c245dc8c3
StenioEric <stenioeric@gmail.com>
2023-10-02 19:24:27 -0300

    Adicionadas assinaturas de funções

--- cliente.h
+++ cliente.h
@@ -13,3 +13,13 @@ void tela_cadastro_cliente(void);
 void tela_pesquisar_cliente(void);
 void tela_alterar_cliente(void);
 void tela_excluir_cliente(void);
+void valCliente (char *nome, char *cpf, char *email, char *dataNasc, char *telefone);
+void limparBuffer(void);
+int validarNome(char *nome);
+int validarCPF(char *cpf);
+int valEmail(char *email);
+int validarData(char *data);
+int validarFone(char *fone);
+
+
+
---
+++ util.h
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Serido                     ///
+///             PROJETO SIG DE UMA LOJA PARA ARTIGOS FEMININOS              ///
+///             Developed by @stenioeric -- since August, 2023              ///
+///////////////////////////////////////////////////////////////////////////////
+
+/// Assinatura das funcoes
+int ehDigito(char);
+int ehLetra(char);
+int ehBissexto(int);
+int ehData(int, int, int);
+int validarNome(char*);
+int valEmail(char*);
+int validarData(char*);
+int validarFone(char*);
+void limparBuffer(void);
