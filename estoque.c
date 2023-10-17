1488c28de34a438272d71a0a08c0bdda9123156e
StenioEric <stenioeric@gmail.com>
2023-10-17 01:40:17 -0300

    Implementado struct

--- estoque.c
+++ estoque.c
@@ -8,11 +8,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include "estoque.h"
+#include "util.h"
 
+typedef struct estoque Estoque;
 
 void moduloEstoque(void) {
     char op;
+    // Estoque* exemplo;
 
     do { 
         op = tela_menu_estoque();
@@ -71,7 +75,8 @@ char tela_menu_estoque(void) {
 
 
 void tela_cadastro_produto(void) {
-    
+    // Estoque* est;
+    // est = (Estoque*)malloc(sizeof(Estoque));
     system("clear||cls");
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
@@ -93,11 +98,14 @@ void tela_cadastro_produto(void) {
     printf("///            QUANTIDADE DE PRODUTOS:                                      ///\n");
     printf("///            VALOR DO PRODUTO:                                            ///\n");
     printf("///            DESCRICAO DO PRODUTO:                                        ///\n");
+    printf("///            ID:                                                          ///\n");
+    // valEstoque(est);
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
+    // return est;
 }
 
 void tela_pesquisar_produto(void) {
@@ -121,13 +129,12 @@ void tela_pesquisar_produto(void) {
     printf("///                         PESQUISAR PRODUTO                               ///\n");
     printf("///              -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-                    ///\n");
     printf("///                                                                         ///\n");
-    printf("///              ID PRODUTO:                                                ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
+
+    // idEstoque(est);
 }
 
 void tela_alterar_produto(void) {
@@ -151,13 +158,11 @@ void tela_alterar_produto(void) {
     printf("///                         ALTERAR PRODUTO                                 ///\n");
     printf("///              -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-                    ///\n");
     printf("///                                                                         ///\n");
-    printf("///              ID PRODUTO:                                                ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
+    // idEstoque(est);
 }
 
 void tela_remover_produto(void) {
@@ -181,11 +186,100 @@ void tela_remover_produto(void) {
     printf("///                         REMOVER PRODUTO                                 ///\n");
     printf("///              -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-                    ///\n");
     printf("///                                                                         ///\n");
-    printf("///              ID PRODUTO:                                                ///\n");
-    printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
+    // idEstoque(est);
 }
+
+///////////////////////////////////////////////////////////////////////////////
+/// Validador de nome, cpf , email , telefone e data de nascimento.
+///
+// void valEstoque (Estoque *est ) {
+
+//     do {
+//         printf("/// PRODUTO:");
+//         scanf("%s", est -> produto);
+//         limparBuffer();
+//     } while(!validarNome(est -> produto));
+
+//     do {
+//         printf("/// QUANTIDADE DE PRODUTOS:");
+//         scanf("%c", est -> quantidade);
+//         limparBuffer();
+//     } while(!ehDigito(est -> quantidade));
+
+//     do {
+//         printf("/// VALOR:");
+//         scanf("%c",est -> valor);
+//         limparBuffer();
+//     } while(!ehDigito(est -> valor));
+
+//     do {
+//         printf("/// DESCRICAO:");
+//         scanf("%s",est -> descricao);
+//         limparBuffer();
+//     } while(!validarNome(est -> descricao));
+
+//     do {
+//         printf("/// ID:");
+//         scanf("%c",est -> id);
+//         limparBuffer();
+//     } while(!ehDigito(est -> id));
+
+// }
+
+// void idEstoque( const Estoque* est) {
+//     char* id;
+//     id = (char*)malloc(sizeof(char));
+
+//     do {
+//         printf("///     ID:");
+//         scanf("%s", id);
+//         limparBuffer();    
+//     } while(!ehDigito(id));
+
+//     if (ehDigito(id) == 1) {
+//         printEstoque(id);
+//     } else {
+//         printf("\n");
+//         printf("///     USUARIO NAO ENCONTRADO");
+//         printf("\n");
+//         printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+//         limparBuffer();
+//     }
+// }
+
+// void printEstoque( const Estoque* est)
+// {
+//     system("clear||cls");
+//     printf("\n");
+//     printf("///////////////////////////////////////////////////////////////////////////////\n");
+//     printf("///                                                                         ///\n");
+//     printf("///        EEEEEEE lll                                                      ///\n");
+//     printf("///        EE      lll   eee   gggggg   aa aa nn nnn    cccc   eee          ///\n");
+//     printf("///        EEEEE   lll ee   e gg   gg  aa aaa nnn  nn cc     ee   e         ///\n");
+//     printf("///        EE      lll eeeee  ggggggg aa  aaa nn   nn cc     eeeee          ///\n");
+//     printf("///        EEEEEEE lll  eeeee      gg  aaa aa nn   nn  ccccc  eeeee         ///\n");
+//     printf("///                             ggggg                                       ///\n");
+//     printf("///                                                                         ///\n");
+//     printf("///                      LOJA DE ARTIGOS FEMININOS                          ///\n");
+//     printf("///                                                                         ///\n");
+//     printf("///////////////////////////////////////////////////////////////////////////////\n");
+//     printf("///                                                                         ///\n");
+//     printf("///                 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-                 ///\n");
+//     printf("///                          PRINT DADOS PRODUTO                            ///\n");
+//     printf("///                 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-                 ///\n");
+//     printf("///                                                                         ///\n");
+//     printf("///            PRODUTO:%s\n", est-> produto);
+//     printf("///            QUANTIDADE:%s\n", est-> quantidade);                                                         
+//     printf("///            VALOR:%s\n", est-> valor);                                                      
+//     printf("///            DESCRICAO:%s\n", est-> descricao);                                          
+//     printf("///            ID: %s\n", est-> id);                    
+//     printf("///                                                                         ///\n");
+//     printf("///                                                                         ///\n");
+//     printf("///                                                                         ///\n");
+//     printf("///////////////////////////////////////////////////////////////////////////////\n");
+//     getchar();
+// }
--- estoque.h
+++ estoque.h
@@ -5,9 +5,25 @@
 ///             Developed by @stenioeric -- since August, 2023              ///
 ///////////////////////////////////////////////////////////////////////////////
 
+/// Assinatura de funcoes
+
+typedef struct estoque Estoque;
+typedef struct estoque {
+    char produto[20];
+    char quantidade[13];
+    char valor[30];
+    char descricao[30];
+    char id[5];
+} Estoque;
+
 void moduloEstoque(void);
 char tela_menu_estoque(void);
 void tela_cadastro_produto(void);
 void tela_pesquisar_produto(void);
 void tela_alterar_produto(void);
 void tela_remover_produto(void);
+// void idEstoque(const Estoque*);
+// void printEstoque(const Estoque*);
+// void limparBuffer(void);
+// void valEstoque (Estoque*);
+
