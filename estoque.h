e7337f9602178831f8297ab7eb6fcd659fe9d163
StenioEric <stenioeric@gmail.com>
2023-10-31 15:26:07 -0300

    Correção do struct

--- estoque.h
+++ estoque.h
@@ -8,17 +8,18 @@
 /// Assinatura de funcoes
 
 typedef struct estoque Estoque;
-typedef struct estoque {
+struct estoque {
     char produto[20];
     char quantidade[13];
     char valor[30];
     char descricao[30];
     char id[5];
-} Estoque;
+    char status;
+};
 
 void moduloEstoque(void);
 char tela_menu_estoque(void);
-void tela_cadastro_produto(void);
+Estoque* tela_cadastro_produto(void);
 void tela_pesquisar_produto(void);
 void tela_alterar_produto(void);
 void tela_remover_produto(void);
